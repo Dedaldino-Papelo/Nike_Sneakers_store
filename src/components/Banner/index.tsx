922dad7526fb841bf0d2cfa666618ebecb3ebfb6
Dedalino <kelsonpapelo18@gmail.com>
2023-07-03 19:38:20 +0100

    FEAT: changing banner image png to svg, added react-native-svg-transformer

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,24 +1,41 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import styles from "./styles";
+import Logo from "../../assets/shoesss-removebg-preview 1.svg";
+import { MotiView } from 'moti'
 
 const index = () => {
   return (
-    <View style={styles.container}>
+    <MotiView 
+      style={styles.container}
+      from={{
+        rotateX: '-100deg',
+        opacity: 0
+      }}
+      animate={{
+        rotateX: '0deg',
+        opacity: 1
+      }} 
+      transition={{
+          type:"timing",
+          duration: 900,
+          delay: 300,
+      }}
+      >
       <View style={styles.texts}>
         <Text style={styles.text}>DON’T TRY</Text>
-        <Text style={[styles.text, {fontSize: 30, fontWeight: '900'}]}>JUST DO IT.</Text>
+        <Text style={[styles.text, { fontSize: 30, fontWeight: "900" }]}>
+          JUST DO IT.
+        </Text>
       </View>
       <View style={styles.ButtonSneaker}>
         <TouchableOpacity style={styles.button}>
-            <Text style={styles.textButton}>Order Now</Text>
-          </TouchableOpacity>
-          <Image 
-            style={styles.image}
-            source={require("../../assets/sneakers.png")} />
+          <Text style={styles.textButton}>Order Now</Text>
+        </TouchableOpacity>
+        <Logo style={styles.image} width={278} height={146} />
       </View>
-    </View>
-  )
-}
+    </MotiView>
+  );
+};
 
-export default index
+export default index;
--- src/components/DetailsHeader/index.tsx
+++ src/components/DetailsHeader/index.tsx
@@ -3,11 +3,24 @@ import React from 'react'
 import styles from './styles';
 import { AntDesign, FontAwesome  } from '@expo/vector-icons';
 import { useNavigation, NavigationProp } from '@react-navigation/native';
+import { MotiView } from 'moti'
 
 const Index = () => {
   const navigation = useNavigation<NavigationProp<any>>();
   return (
-    <View style={styles.container}>
+    <MotiView
+    from={{
+      translateX: -300,
+    }} 
+    animate={{
+      translateX: 0,
+    }}
+    transition={{
+      type:"timing",
+      duration: 800,
+      delay: 350,
+    }}
+      style={styles.container}>
         <AntDesign 
             onPress={() => navigation.goBack() }
             name="left" 
@@ -19,7 +32,7 @@ const Index = () => {
             name="shopping-bag" 
             size={24} 
             color="#475F7D" />
-    </View>
+    </MotiView>
   )
 }
 
